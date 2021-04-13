ea9528d3d82ce550819c6f8a99fe9fcfd7067254
Mish Ochu <mishochu@gmail.com>
2021-04-13 10:13:41 -0600

    use a  timer

--- IP_Address_Check/Worker.cs
+++ IP_Address_Check/Worker.cs
@@ -4,28 +4,51 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading;
+using System.Timers;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace IP_Address_Check
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly System.Timers.Timer _timer;
+        private const int thirtySeconds = 30000;
 
         public Worker(ILogger<Worker> logger)
         {
+
             _logger = logger;
+            _timer = new System.Timers.Timer(thirtySeconds);
+            _timer.Elapsed += OnIntervalElapsed;
+        }
+
+        private void OnIntervalElapsed(object sender, ElapsedEventArgs e)
+        {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                _logger.LogInformation(GetIp.Check());
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                _logger.LogInformation(GetIp.Check());
-                await Task.Delay(30000, stoppingToken);
-            }
+            OnIntervalElapsed(null, null);
+            _timer.Start();
+
+            await Task.Run(() => stoppingToken.Register(() => { _timer.Stop(); }));
+        }
+
+        public override void Dispose()
+        {
+            _timer.Dispose();
+            base.Dispose();
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            _timer.Stop();
+            return base.StopAsync(cancellationToken);
         }
     }
+
 }
